6ed57cae9f193257bb3f13ef76c7cc2a015b6006
Eric Margules <eric.i.margules@gmail.com>
2017-03-21 22:55:19 -0500

    remove opacity javascript for portfolio

--- portfolio.js
+++ portfolio.js
@@ -47,19 +47,6 @@ function handlePortfolio() {
 }
 
 $(document).ready(function(){
-
-	$(".portfolio_item").mouseenter(function(){
-
-		$(this).children("*").children(".block").css("opacity", "0.8");
-
-	});
-
-	$(".portfolio_item").mouseleave(function(){
-
-		$(this).children("*").children(".block").css("opacity", "1");
-
-	});
-
 	
 	handlePortfolio();
 
