c0be75a248b62d43cf6e65755b559d7fda1354a1
Eric Margules <eric.i.margules@gmail.com>
2017-04-30 18:53:06 -0500

    add scroll effect to hero mockup

--- main_mockup2.js
+++ main_mockup2.js
@@ -3,20 +3,18 @@ $(window).scroll(function() {
         $('.top_nav').removeClass('home');
         $('.top_nav').addClass('little');
         $("li > a").addClass('small_link');
-        // $('.top').addClass('appear');
         $(".top_nav > nav > li > a").removeClass('white');
         $(".top_nav > nav > li > a").addClass('black');
     } else {
         $('.top_nav').addClass('home');
         $('.top_nav').removeClass('little');
         $("li > a").removeClass('small_link');
-        // $('.top').removeClass('appear');
         $(".top_nav > nav > li > a").addClass('white');
     }
 });
-// $(window).scroll(function() {
-//     var scrollPos = -20 + $(window).scrollTop() * (-1) * 0.5;
-//     if ($(window).scrollTop() < $(window).height() * 0.7) {
-//         $("#content").css('margin-top', scrollPos + "px");
-//     }
-// });
+$(window).scroll(function() {
+    if($(window).scrollTop() > 0) {
+        var top = Math.round(($(window).scrollTop() / 6));
+        $('#hero')[0].style.opacity = (1 -(top * .01));
+    }
+});
